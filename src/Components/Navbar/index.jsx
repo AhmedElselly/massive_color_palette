cbce09a3839a688610dce1f9b0926b7fd47c71cb
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-08-30 11:17:54 +0300

    added format change in navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import styles from '../../styles/Navbar.module.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
-const Navbar = ({level, handleChangeLevel}) => {
+const Navbar = ({ level, handleChangeLevel, handleSelectedFormat }) => {
 	const [newLevel, setNewLevel] = useState(level);
-  const handleSliderChange = val => {
+	const [selected, setSelected] = useState('hex');
+	const handleSliderChange = val => {
 		setNewLevel(val)
-    handleChangeLevel(val);
-  }
+		handleChangeLevel(val);
+	}
+
+	const handleSelectChange = e => {
+		setSelected(e.target.value);
+		handleSelectedFormat(e.target.value);
+	}
 
 	return (
 		<div className={styles.container}>
@@ -18,6 +25,20 @@ const Navbar = ({level, handleChangeLevel}) => {
 					<span className={styles.levelText}>Level:{newLevel}</span>
 					<Slider defaultValue={level} min={100} max={900} onChange={handleSliderChange} step={100} />
 				</div>
+			</div>
+			<div className={styles.right}>
+				<FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+					<Select
+						labelId="demo-simple-select-standard-label"
+						id="demo-simple-select-standard"
+						value={selected}
+						onChange={handleSelectChange}
+					>
+						<MenuItem value={'hex'}>HEX - #ffffff</MenuItem>
+						<MenuItem value={'rgb'}>RGB - {`rgb(255,255,255)`}</MenuItem>
+						<MenuItem value={'rgba'}>RGBA - {`rgb(255,255,255,1)`}</MenuItem>
+					</Select>
+				</FormControl>
 			</div>
 		</div>
 	)
--- src/Components/Palette/index.jsx
+++ src/Components/Palette/index.jsx
@@ -5,19 +5,24 @@ import Navbar from '../Navbar';
 
 const Palette = ({ palette }) => {
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState('hex');
 
   const handleSliderChange = val => {
     setLevel(val);
   }
 
+  const handleSelectedFormat = val => {
+    setFormat(val);
+  }
+
   const generateColorBoxes = () => palette.colors[level].map((color, i) => {
     return (
-      <ColorBox key={i} background={color.rgba} name={color.name} />
+      <ColorBox key={i} background={color[format]}  name={color.name} />
     )
   })
   return (
     <div className={styles.palette}>
-      <Navbar level={level} handleChangeLevel={handleSliderChange} />
+      <Navbar level={level} handleChangeLevel={handleSliderChange} handleSelectedFormat={handleSelectedFormat} />
       <div className={styles.paletteColors}>
         {generateColorBoxes()}
       </div>
