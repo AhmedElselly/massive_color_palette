14be3152305387258cc17bc4c69fbd44bf988527
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-09-03 12:48:45 +0300

    adjust the text color based on the luminance of background ColorBox component

--- src/Components/ColorBox/index.jsx
+++ src/Components/ColorBox/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, useEffect, useState } from 'react';
 import styles from '../../styles/ColorBox.module.css'
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 
 const ColorBox = ({ name, background, colorId, paletteId, showMore, fullHeight }) => {
 	const [copied, setCopied] = useState(false);
@@ -14,22 +15,23 @@ const ColorBox = ({ name, background, colorId, paletteId, showMore, fullHeight }
 	const changeCopyState = () => {
 		setCopied(true)
 	}
+	const isDark = chroma(background).luminance() <= 0.08;
 	return (
 		<CopyToClipboard text={background} onCopy={changeCopyState}>
 			<div style={{ background }} className={!fullHeight ? styles.colorBox : `${styles.colorBox} ${styles.colorBoxFullHeight}`}>
 				<div style={{ background }} className={`${styles.copyOverlay} ${copied && styles.show}`} />
-				<div className={`${styles.copyMsg} ${copied && styles.show}`}>
+				<div className={`${styles.copyMsg} ${copied && styles.show} ${isDark ? 'lightText' : 'darkText'}`}>
 					<h1 className={styles.copyText}>copied!</h1>
 					<p className={styles.colorName}>{background}</p>
 				</div>
 				<div className={styles.copyContainer}>
 					<div className={styles.boxContent}>
-						<span>{name}</span>
+						<span className={isDark ? 'lightText' : 'darkText'}>{name}</span>
 					</div>
 					<button className={styles.copyBtn}>COPY</button>
 				</div>
 				{showMore && <Link to={`/palettes/${paletteId}/${colorId}`} onClick={(e) => e.stopPropagation()}>
-					<span className={styles.seeMore}>MORE</span>
+					<span className={`${styles.seeMore} ${isDark ? 'lightText' : 'darkText'}`}>MORE</span>
 				</Link>}
 			</div>
 		</CopyToClipboard>
