58d374e1bb1e1c99c233ffdcdf0dd15dda388af5
ahmedelselly <ahmedelselly87@gmail.com>
2024-02-13 16:55:03 +0200

    modified container

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { useLocation, useOutlet } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import '../../Animation.css'
 
-const Container = ({ children }) => {
-	const location = useLocation()
-	const currentOutlet = useOutlet()
+const index = ({children}) => {
+	const location = useLocation();
 	return (
-		<TransitionGroup>
-			<CSSTransition
-				key={location.pathname}
-				timeout={500}
-				classNames="page"
-				unmountOnExit
-			>
-				<div className="page">
-					{children}
-				</div>
-			</CSSTransition>
-		</TransitionGroup>
+		<div>
+			<TransitionGroup>
+				<CSSTransition
+					key={location.pathname}
+					timeout={500}
+					classNames="page"
+					unmountOnExit
+				>
+					<div className="page">
+						{children}
+					</div>
+				</CSSTransition>
+			</TransitionGroup>
+		</div>
 	)
 }
 
-export default Container
+export default index
